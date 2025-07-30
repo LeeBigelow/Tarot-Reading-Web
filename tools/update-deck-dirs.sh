9525968ea76d683807b2cb03a45b2b68bd5824ac
Lee Bigelow <ligelowbee@gmail.com>
2025-07-30 01:40:42 -0230

    automate updating available layouts

--- tools/update-deck-dirs.sh
+++ tools/update-deck-dirs.sh
@@ -4,8 +4,7 @@ imgd="../resources/images/"
 oldf="../resources/js/deal.js"
 newf="new-deal.js"
 sed -e '/DECKS START/q' $oldf > $newf
-find $imgd -mindepth 1 -maxdepth 1 \( -type d -o -type l \) -printf "%f\n" \
-	| sed "s/^/\t'/; s/$/',/" \
+find $imgd -mindepth 1 -maxdepth 1 \( -type d -o -type l \) -printf "\t'%f',\n" \
 	| sort >> $newf
 sed -n '/DECKS END/,$p' $oldf >> $newf
 mv $oldf $newf.$EPOCHSECONDS.bak
